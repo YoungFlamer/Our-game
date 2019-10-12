789002c920995d5879926fac52954f7f63d6a772
Timofey Kondratenko <tiko2yt@gmail.com>
2019-10-12 18:54:42 +0300

    camera moving when enter new room

---
+++ TDS/Assets/Scripts/RoomScripyt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScripyt : MonoBehaviour
+{
+    
+    public Vector3 offset;
+    public GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // todo: check the player
+
+        player = GameObject.FindWithTag("MainCharacter");
+        if(player == other.gameObject)
+        {
+            Camera.main.transform.position = transform.position + offset;
+        }
+    }
+
+}
