57295f835624a83fe9fe99a6ffa8ad728c97707f
YoungFlamer <serhii_iyevlyev@ukr.net>
2019-11-15 22:08:22 +0200

    enemy in room spawn scripts added

---
+++ Assets/Scripts/EnemyInRoomGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInRoomGenerator : MonoBehaviour
+{
+	public GameObject[] enemyPrefs;
+	public Transform[] spawnPoints;
+	public bool firstTime = true;
+	public void SpawnEnemies(){
+		if (firstTime){
+		    for (int i = 0; i < spawnPoints.Length; i++){
+			    Instantiate(enemyPrefs[Random.Range(0, enemyPrefs.Length - 1)], spawnPoints[i].position, Quaternion.identity);
+		    }
+			firstTime = false;
+		}
+	}
+}
---
+++ Assets/Scripts/RoomEnterer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomEnterer : MonoBehaviour
+{
+	public string roomTag;
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == roomTag){
+			col.gameObject.GetComponent<EnemyInRoomGenerator>().SpawnEnemies();
+		}
+	}
+}
