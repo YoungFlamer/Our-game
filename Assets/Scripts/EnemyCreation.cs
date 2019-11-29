05e81ef1d1b7960f055099adec1311a9af77391f
YoungFlamer <serhii_iyevlyev@ukr.net>
2019-11-29 23:07:49 +0200

    enemy in room generation

--- Assets/Scripts/EnemyCreation.cs
+++ Assets/Scripts/EnemyCreation.cs
@@ -2,16 +2,36 @@
 
 public class EnemyCreation : MonoBehaviour
 {
-    public GameObject[] Enemies = new GameObject[4];
+    public GameObject[] Enemies;
+	public Transform[] spawnPoints;
+	public GameObject hero;
+	private bool acted = false;
     
     void Start()
     {
-        Random rand = new Random();
-        int a = Random.Range(0, 4);
-        GameObject Enemy = Instantiate(Enemies[a]);
-       
-        GameObject startpref = GameObject.Find("AbstractLevelGeneration");
-        LevelGeneration level = startpref.GetComponent<LevelGeneration>();
-        Enemy.transform.position = level.GetSpawnPosition() + new Vector3(-10, 10, -10);//level.intersectionRooms[1].transform.position + new Vector3(10,0,10);    
+		hero = GameObject.FindWithTag("MainCharacter");
     }
+
+	void CreateEnemies(){
+		int n = spawnPoints.Length;
+		for (int i = 0; i < n; i++){
+			int c = Random.Range(0, Enemies.Length);
+			Instantiate(Enemies[c], spawnPoints[n].position, Quaternion.identity);
+		}
+	}
+
+	void Update(){
+		if (!acted){
+			CheckPlayerInRoom();
+		}
+	}
+
+	void CheckPlayerInRoom(){
+		float xDistance = hero.transform.position.x - transform.position.x;
+		float zDistance = hero.transform.position.z - transform.position.z;
+		if (Mathf.Abs(zDistance) < 15 || Mathf.Abs(xDistance) < 15){
+			CreateEnemies();
+		}
+		acted = false;
+	}
 }
