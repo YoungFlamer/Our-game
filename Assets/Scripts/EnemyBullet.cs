edd95adbd128bb5645194f1e7b4e22d2ac8b316f
YoungFlamer <serhii_iyevlyev@ukr.net>
2019-12-06 23:46:18 +0200

    Shooting enemies added

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+	public DamageData dd;
+	public float speed;
+	public float flyDistance;
+    // Update is called once per frame
+    void Update()
+    {
+		float s = speed * Time.deltaTime;
+		flyDistance -= s;
+		transform.Translate(transform.forward * s);
+		if (flyDistance < 0){
+			Destroy(gameObject);
+		}
+    }
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "MainCharacter"){
+			col.gameObject.GetComponent<HeroDamageSystem>().TakeDamage(dd);
+		}
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/ShootingEnemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : Enemy
+{
+	public float speed;
+	public float reloadTime;
+	private float reloadingTimer;
+	public GameObject projectile;
+	public Transform firePoint;
+    // Start is called before the first frame update
+    public override void Start()
+    {
+		base.Start();
+    }
+
+    // Update is called once per frame
+	void Update()
+	{
+		if (player != null && !isSleeping)
+		{
+			float distance = Vector3.Distance(transform.position, player.transform.position);
+
+			if (distance <= visionDistance && distance >= attackDistance)
+			{
+				Move();
+			}
+			if (distance <= attackDistance && reloadingTimer >= reloadTime)
+			{
+				Attack();
+				reloadingTimer = 0;
+			}
+
+			if (reloadingTimer < reloadTime)
+			{
+				reloadingTimer += Time.deltaTime;
+			}
+		}
+	}
+	public override void Attack(){
+		transform.LookAt(player.transform.position);
+		GameObject d = Instantiate (projectile, firePoint.position, Quaternion.identity);
+		d.transform.LookAt (player.transform.position);
+
+	}
+	override public void Move()
+	{
+		transform.LookAt(player.transform.position);
+		m_rigidbody.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+	}
+}
